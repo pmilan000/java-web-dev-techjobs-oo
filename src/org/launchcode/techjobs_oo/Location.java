f3318d9cf3a14e542851c904288ce7681ee48013
Milani <PMilan000@cable.comcast.com>
2020-05-05 20:23:16 -0400

    Java-Assignment2

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -12,7 +12,11 @@ public class Location {
         nextId++;
     }
 
-    // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
+    public Location(int id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+// TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
 
